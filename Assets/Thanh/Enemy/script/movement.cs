c7defdfb2e3306403f3ae1a36ed2ff2edea02cfc
Tam Doan <118838652+tamhoccodegame@users.noreply.github.com>
2024-05-28 14:10:03 +0700

    update

--- Assets/Thanh/Enemy/script/movement.cs
+++ Assets/Thanh/Enemy/script/movement.cs
@@ -7,23 +7,75 @@ public class movement : MonoBehaviour
 {
 
     [SerializeField] float moveSpeed = 1f;
+    [SerializeField] float detectRange = 5f;
     private Rigidbody2D _rigidbody2D;
+
+    public GameObject startPoint;
+    public GameObject endPoint;
+    private Vector3 initPos;
+    float direction = 1f;
+
+    private bool isChasing = false;
+    private Transform player;
+
     // Start is called before the first frame update
     void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        player = GameObject.Find("Player").GetComponent<Transform>();
+        initPos = player.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        _rigidbody2D.velocity = new Vector2(x: moveSpeed, y: 0);
+
+
+        direction = transform.position.x - player.transform.position.x;
+
+		if (Mathf.Abs(direction) < detectRange)
+        {
+            //Di player
+            if(Mathf.Abs(transform.position.y - player.transform.position.y) <= 1)
+            {
+                isChasing = true;
+            }
+        }
+
+        if(isChasing)
+        {
+
+			if (Mathf.Abs(direction) >= 7)
+			{
+				isChasing = false;
+
+			}
+
+			Vector3 newPos = transform.position;
+            newPos.x += -direction * moveSpeed * Time.deltaTime;
+
+            transform.position = newPos;
+
+            
+        }
+
+        else
+        {
+			_rigidbody2D.velocity = new Vector2(x: direction * moveSpeed, y: 0);
+		}
+
     }
 
-    private void OnTriggerExit2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        moveSpeed *= -1;
+        //neu cham vao 1 trong 2 diem thi quay dau
+        if (other.gameObject == startPoint)
+            direction = 1;
+
+        if (other.gameObject == endPoint)
+            direction = -1; 
+
         //xoay huong cua slime
-        transform.localScale = new Vector2(x: -(Mathf.Sign(_rigidbody2D.velocity.x)),y:1f);
+        transform.localScale = new Vector2(x: -(Mathf.Sign(_rigidbody2D.velocity.x)), y: 1f);
     }
 }
